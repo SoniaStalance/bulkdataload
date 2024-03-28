6dc60c618ec0a0ea9d6ab3a8a61230c4427d7db3
SoniaStalance <soniastalance@gmail.com>
2024-03-28 21:58:52 +0530

    boilerplate

---
+++ src/main/java/com/learning/bulkdataload/BulkdataloadApplication.java
@@ -0,0 +1,13 @@
+package com.learning.bulkdataload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkdataloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkdataloadApplication.class, args);
+	}
+
+}
